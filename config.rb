af3bbe46e72f2d9ebc5e4a39f799dcff4e55701b
Tobias Hinz <hinz@uni-bremen.de>
2014-01-11 15:32:02 +0100

    Using SCSS for CSS styling

---
+++ config.rb
@@ -0,0 +1,12 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+line_comments = false # by Compass.app 
+
+output_style = :compressed # by Compass.app 
